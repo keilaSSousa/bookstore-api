9f00039bd0b364febde4af7a36f9bc2c95d51155
keilaSSousa <keilars10@gmail.com>
2022-04-17 14:45:18 -0300

    Listando todos os livros por Categoria

--- src/main/java/com/keila/bookstore/resources/LivroResourse.java
+++ src/main/java/com/keila/bookstore/resources/LivroResourse.java
@@ -1,13 +1,18 @@
 package com.keila.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keila.bookstore.dominio.Livro;
+import com.keila.bookstore.dtos.LivroDTO;
 import com.keila.bookstore.service.LivroService;
 
 @RestController
@@ -22,6 +27,18 @@ public class LivroResourse {
 		
 		Livro obj= livroService.findById(id);
 		return ResponseEntity.ok().body(obj);	
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value="categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> livros = livroService.findAll(id_cat);
+		
+		List<LivroDTO> listDto = livros.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
+		
+		//localhost:8080/livros?categoria=1
 		
 	}
 }
--- src/main/java/com/keila/bookstore/service/LivroService.java
+++ src/main/java/com/keila/bookstore/service/LivroService.java
@@ -1,11 +1,13 @@
 package com.keila.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.keila.bookstore.dominio.Livro;
+import com.keila.bookstore.repositories.CategoriaRepository;
 import com.keila.bookstore.repositories.LivroRepository;
 import com.keila.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -15,11 +17,22 @@ public class LivroService {
 	@Autowired
 	private LivroRepository livroRepository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = livroRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + ", Tipo: " + Livro.class.getName()));
 		
 	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		
+		categoriaService.findById(id_cat);
+				
+		return livroRepository.findAllByCategoria(id_cat);
+	}
+	
 
 }
